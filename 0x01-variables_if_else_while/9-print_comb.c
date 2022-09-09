fc7aa94426b3372a0d12c7f46581d59981d79244
ololade <ololadelawal@gmail.com>
2022-09-09 14:01:50 -0700

    print comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: prints all possible combinations of single-digit numbers,
+ * Numbers must be separated by ,(44) followed by a space (32)
+ * Return: 0
+ */
+int main(void)
+{
+int n;
+for (n = 0; n < 10; n++)
+{
+putchar(n + '0');
+{
+putchar(44);
+putchar(32);
+}
+}
+putchar('\n');
+return (0);
+}
