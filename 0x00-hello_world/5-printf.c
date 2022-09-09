c78d933834bbcec52e5a835bf155a0c5d8a3ca6b
ololade <ololadelawal@gmail.com>
2022-09-08 17:45:26 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,8 +3,8 @@
 /**
  * main - entry point
  *
- * Description: "prints with proper grammar, but the outcome is a piece of art"
- *
+ * Description: "prints with proper grammar, but the outcome is a piece
+ * of art, "
  * Return: 0 (success)
  */
 int main(void)
