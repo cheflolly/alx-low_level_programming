e892e5c2ca0879b9e5647c89b055b8922e9612e3
ololade <ololadelawal@gmail.com>
2022-09-09 12:16:00 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - main file
+ * description: print 0-9
+ * return: 0
+ */
+int main(void)
+{
+int n;
+for (n = ; n <= 9; n++)
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
+}
