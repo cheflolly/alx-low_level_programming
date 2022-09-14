fa47ba03b94a5f9501c5f4640d75f860c2302f85
ololade <ololadelawal@gmail.com>
2022-09-14 06:34:09 -0700

    update

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -5,7 +5,7 @@
  * Return: 0
  */
 
-int main(void) 
+int main(void)
 {
 int i;
 
@@ -15,10 +15,10 @@ i = 0;
  /**
  * while (i < 10)
  *  {
- *  putchar(i); 
+ *  putchar(i);
  * }
  */
 printf("Infinite loop avoided! \\o/\n");
 
-return (0); 
+return (0);
 }
