8627bfa6755ff03603d50a5d7ca57724f82b39f0
ololade <ololadelawal@gmail.com>
2022-09-15 12:57:15 -0700

    0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints 0 (ascii 48) to 9 (ascii 57)
+ * Return: nothing
+ */
+
+void print_numbers(void)
+{
+int n;
+for (n = 48; n <= 57; n++)
+{
+_putchar(n);
+}
+_putchar('\n');
+}
