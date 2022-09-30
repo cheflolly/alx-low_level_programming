2330ebc0573979d9e3199223d43d06c10dfc4749
ololade <ololadelawal@gmail.com>
2022-09-30 15:44:00 -0700

    0x0A-3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers
+ * @argc: argument count
+ * @argv: argument vector, array of strings
+ * Return: 0 if successful, otherwise 1
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	if (argv[1] && argv[2])
+	{
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+		return (0);
+	}
+	else
+		printf("Error\n");
+	return (1);
+}
