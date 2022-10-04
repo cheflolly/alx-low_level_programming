4a3a8b577c242d60a9937b709580d8c2bd3d7c23
ololade <ololadelawal@gmail.com>
2022-10-04 15:02:25 -0700

    0x0B-2

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - create a 2d integer grid
+ * @width: int sie width
+ * @height: int size height
+ * Return: pointer to a new grid
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int 
+}
