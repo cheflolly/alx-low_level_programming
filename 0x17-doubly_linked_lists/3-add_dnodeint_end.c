6e6d0fb85e6b069f2faaf9196c99e1e44d63eaa4
ololade <ololadelawal@gmail.com>
2022-12-09 10:21:12 -0800

    17x03

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -10,8 +10,8 @@
 
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *new;
 	dlistint_t *h;
+	dlistint_t *new;
 
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
@@ -19,12 +19,13 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 
 	new->n = n;
 	new->next = NULL;
+
 	h = *head;
 
 	if (h != NULL)
 	{
 		while (h->next != NULL)
-			h = h-next;
+			h = h->next;
 		h->next = new;
 	}
 	else
